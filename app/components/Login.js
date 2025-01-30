409812a3b76b1ce1d47439236db334e4176ae2cd
Fabio Squizzato <fsquizzato@visualmedica.com>
2025-01-30 16:19:05 -0300

    first commit

---
+++ app/components/Login.js
@@ -0,0 +1,12 @@
+import styles from './Login.module.css';
+
+export default function Login(){
+
+
+    return (
+        <div className={styles.main}>
+            <h1>Loggin</h1>
+
+        </div>
+    )
+}
