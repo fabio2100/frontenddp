2037f5ea17c8a7eb0c62af6ace5babe51f58a39a
Fabio Squizzato <fsquizzato@visualmedica.com>
2025-01-30 17:57:35 -0300

    update: login page

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import styles from './Login.module.css';
+import { TextField } from '@mui/material';
 
-export default function Login(){
+export default function Login({setLoggin}) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
+    const handleSubmit = async (e) => {
+        setLoggin(true)
+    }
+    
 
     return (
         <div className={styles.main}>
-            <h1>Loggin</h1>
-
+            <TextField id="outlined-basic" label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)}></TextField>
+            <TextField id="outlined-basic" label="Password" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)}></TextField>  
+            <button onClick={handleSubmit} className={styles.btn}>Login</button>
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,17 +1,26 @@
 'use client'
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Login from './components/Login';
 import User from './components/User';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
 
 export default function Home() {
 
   const [loggin,setLoggin] = useState(false);
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+
   return (
-    <div>
-      {loggin ? <User /> : <Login />}
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      {loggin ? <User /> : <Login setLoggin={setLoggin}/>}
+      </ThemeProvider>  
   );
 }
